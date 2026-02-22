1615ac3409083ff3229ddc738d6f092aac5e3f4e
Uriel Bajada <ubajada@stcmalta.edu.mt>
2026-02-22 10:39:07 +0100

    Add ReportRepository, ReportController, Dependency Inversion Configured SwaggerUI, POST api/reports tested

---
+++ whistleblowerplatform/WhistleblowerPlatform.WebApi/Controllers/ReportController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WhistleblowerPlatform.Application.DTOs;
+using WhistleblowerPlatform.Application.UseCases;
+
+namespace WhistleblowerPlatform.WebApi.Controllers;
+
+[ApiController]
+[Route("api/reports")]
+public class ReportController : ControllerBase
+{
+    private readonly SubmitReportUseCase _submitReportUseCase;
+
+    public ReportController(SubmitReportUseCase submitReportUseCase)
+    {
+        _submitReportUseCase = submitReportUseCase;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> SubmitReport([FromBody] SubmitReportRequest request)
+    {
+        var result = await _submitReportUseCase.ExecuteAsync(request);
+        return Created($"/api/reports/{result.CaseNumber}", result);
+    }
+}
--- whistleblowerplatform/WhistleblowerPlatform.WebApi/Program.cs
+++ whistleblowerplatform/WhistleblowerPlatform.WebApi/Program.cs
@@ -1,17 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using WhistleblowerPlatform.Application.Interfaces;
+using WhistleblowerPlatform.Application.UseCases;
+using WhistleblowerPlatform.Infrastructure.Persistence;
+using WhistleblowerPlatform.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddSwaggerGen();
+// Database
+builder.Services.AddDbContext<WhistleblowerDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Repositories
+builder.Services.AddScoped<IReportRepository, ReportRepository>();
+
+// Use cases
+builder.Services.AddScoped<SubmitReportUseCase>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
