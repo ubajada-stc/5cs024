19511b4efb116180d4f7410284c8507d18722471
Uriel Bajada <ubajada@stcmalta.edu.mt>
2026-03-06 20:36:55 +0100

    Fix HttpClient base address detection for Azure with Development mode

--- WhistleblowerPlatform/WhistleblowerPlatform.Client/Program.cs
+++ WhistleblowerPlatform/WhistleblowerPlatform.Client/Program.cs
@@ -5,7 +5,16 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-if (builder.HostEnvironment.IsDevelopment())
+//if (builder.HostEnvironment.IsDevelopment())
+//{
+//    builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7178/") });
+//}
+//else
+//{
+//    builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//}
+
+if (builder.HostEnvironment.BaseAddress.Contains("localhost"))
 {
     builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7178/") });
 }
